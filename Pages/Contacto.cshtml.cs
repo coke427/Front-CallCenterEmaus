0339acf58a1f8d31274ec975bcf7fea7681e3702
Jorge Mantilla <cokemanti@hotmail.com>
2023-05-19 13:13:04 -0500

    Pages Contacto & Nosotros

---
+++ Pages/Contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace prueba.Pages
+{
+    public class ContactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
