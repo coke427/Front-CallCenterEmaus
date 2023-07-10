91c7639214368d996987f3a3c6fa1e9b9c292bf3
Jorge Mantilla <cokemanti@hotmail.com>
2023-07-10 17:50:27 -0500

    Login y corrección de algunos errores

---
+++ wwwroot/login/js/login.js
@@ -0,0 +1,38 @@
+﻿function IniciarSesion() {
+    var endpoint = hostApi + "/login/listaUsuarios";
+    var email = $("#input_user_email").val();
+    var password = $("#input_user_password").val();
+
+    $.ajax({
+        type: "GET",
+        async: true,
+        url: endpoint,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        dataType: "json",
+        beforeSend: function (xhr) {
+            console.log("Cargando...");
+        },
+        success: function (data) {
+            console.log("Entrando a la función");
+
+            // Verificar las credenciales del usuario
+            var usuarioEncontrado = data[0]; // Acceder al primer elemento del array
+
+            // Verificar el estado del usuario
+            if (usuarioEncontrado && usuarioEncontrado.user_email === email && usuarioEncontrado.user_password === password && usuarioEncontrado.user_status === "A") {
+                console.log("Acceso concedido");
+                window.location.href = "https://localhost:7046/solicitudes";
+            } else {
+                console.log("Credenciales inválidas o usuario inactivo");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log("Error al obtener la lista de usuarios");
+        },
+        fail: function (xhr, status, error) {
+            console.log("Error en la solicitud AJAX");
+        }
+    });
+}
